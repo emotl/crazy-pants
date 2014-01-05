7d3a4f9342c759c2ac043ee7a6304839e63dad51
xrabohrok <westin007@gmail.com>
2014-01-04 20:58:49 -0600

    Cleaned uneeded files

--- GameApp/Game.cpp
+++ GameApp/Game.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "Game.h"
 
 void Game::Start( void ) {
--- GameApp/GameApp.cpp
+++ GameApp/GameApp.cpp
@@ -1,10 +1,9 @@
 // GameApp.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include "Game.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, _TCHAR* argv[])
 {
 	Game::Start();
 
